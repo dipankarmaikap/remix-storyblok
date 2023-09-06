64d350a11c6f4f1a75cf0ebb0bb10d0ea8ca66b8
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2023-09-06 20:04:36 +0530

    This should be fixed

--- app/routes/$.jsx
+++ app/routes/$.jsx
@@ -27,16 +27,12 @@ export const loader = async ({ params, request }) => {
 // }
 export default function Page() {
   const data = useLoaderData()
-  data.story = useStoryblokState(data?.story)
-  if (data?.story) {
-    return (
-      <StoryblokComponent
-        story={data.story}
-        blok={data.story.content}
-        preview={data.isPreview}
-      />
-    )
-  } else {
-    return <></>
-  }
+  let story = useStoryblokState(data.story)
+  return (
+    <StoryblokComponent
+      story={story}
+      blok={story.content}
+      preview={data.isPreview}
+    />
+  )
 }
